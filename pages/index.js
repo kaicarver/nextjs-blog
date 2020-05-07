166e99a6873536935cb52fb630c2370c73678f96
Kai Carver <kaicarver@gmail.com>
2020-05-07 20:21:36 +0200

    Kai strong

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi I'm Kai, learning Next.</p>
+        <p>Hi I'm <strong>Kai</strong>, learning Next.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
